11ce33402c311ba1400d0393dac0d7a8bcdd0acb
sreehari <aesreehari@gmail.com>
2025-10-17 14:37:38 +0530

    Update 5.cpp

--- 5.cpp
+++ 5.cpp
@@ -1,21 +1,24 @@
-// 5. Program for areas using function overloading
+//  Program for areas using function overloading
 #include <iostream>
 using namespace std;
 
 double area(double r) { // Circle
     return 3.14159 * r * r;
 }
+ // Square, using char to differentiate
 
-double area(double s, char type) { // Square, using char to differentiate
+double area(double s, char type) {
     if (type == 's') return s * s;
     return 0;
 }
+// Rectangle
 
-double area(double l, double w) { // Rectangle
+double area(double l, double w) { 
     return l * w;
 }
+// Triangle, using int to differentiate
 
-double area(double b, double h, int type) { // Triangle, using int to differentiate
+double area(double b, double h, int type) { 
     if (type == 1) return 0.5 * b * h;
     return 0;
 }
@@ -38,4 +41,5 @@ int main() {
     cin >> b >> h;
     cout << "Area of triangle: " << area(b, h, 1) << endl;
     return 0;
+
 }
