9514b9f2111f071c2b3128b89859b5ac16dfeedb
sreehari <aesreehari@gmail.com>
2025-10-17 11:23:12 +0530

    Create 1.cpp

---
+++ 1.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Car {
+private:
+    string brand;
+    string model;
+    int year;
+
+public:
+    // Function to read data members
+    void readData() {
+        cout << "Enter Brand: ";
+        getline(cin, brand);
+        cout << "Enter Model: ";
+        getline(cin, model);
+        cout << "Enter Year: ";
+        cin >> year;
+        cin.ignore(); // Clear input buffer
+    }
+
+    // Function to display car details
+    void displayDetails() {
+        cout << "Brand: " << brand << ", Model: " << model << ", Year: " << year << endl;
+    }
+};
+
+int main() {
+    Car car1, car2, car3;
+
+    cout << "Enter details for Car 1:\n";
+    car1.readData();
+    cout << "\nEnter details for Car 2:\n";
+    car2.readData();
+    cout << "\nEnter details for Car 3:\n";
+    car3.readData();
+
+    cout << "\nCar Details:\n";
+    car1.displayDetails();
+    car2.displayDetails();
+    car3.displayDetails();
+
+    return 0;
+}
